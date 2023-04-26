5db0e9a74dafd9027de44000ecc49efe06d44b3f
Ortiz <ortizdcj@gmail.com>
2023-04-26 17:17:08 -0400

    add api project

---
+++ Exercise3_ContactManager/src/Data/Infrastructure/UserManagerDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+	public class UserManagerDbContext : DbContext
+	{
+		#region entities
+		public virtual DbSet<Contact> Contacts { get; set; }
+		public virtual DbSet<User> Users { get; set; }
+		#endregion
+
+		#region constructors
+		public UserManagerDbContext(DbContextOptions options) : base(options) { }
+		#endregion
+	}
+}
